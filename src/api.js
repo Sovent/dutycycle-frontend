fa58eaaa25d03842f47751251d07f8b4bc9b681e
Sovent <vmargelov@gmail.com>
2021-02-17 20:20:07 +0800

    add sign up page

---
+++ src/api.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const apiBaseUrl = "http://localhost:5000";
+
+const instance = axios.create({
+  baseURL: apiBaseUrl
+});
+
+export const createOrganization = (organizationName, adminEmail, adminPassword) =>
+  instance
+  .post("organizations", { Name: organizationName, AdminCredentials: { Email: adminEmail, Password: adminPassword } })
+  .then(response => response.data);
